5fc6ee59161b8e5fa94e86c56ae77f34ab30e49d
github username <rvtingling@yahoo.com>
2021-03-14 23:01:34 -0400

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,20 @@
 // Code SimpleComponent Here
+
+import { render } from "enzyme";
+import React, { Component } from "react";
+export default class SimpleComponent extends Component {
+    constructor() {
+        super();
+        this.state = {mood: "happy" };
+    }
+
+    handleClick = () => {
+        this.setState(previousState => {
+            return { mood: previousState.mood === "happy" ? "sad" : "happy" };
+        });
+    };
+
+    render() {
+        return <div onClick={this.handleClick}>{this.state.mood}</div>;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={() => {document.title = "Now I am sad"}} />
   </div>,
   document.getElementById('root')
 );
